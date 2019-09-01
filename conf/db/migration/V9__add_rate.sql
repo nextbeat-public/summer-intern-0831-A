1d0298117f4b197e78069187dd94791decff2c69
n-a-o-k-i <nao.rami.1124@i.softbank.jp>
2019-09-01 17:36:19 +0900

    カラム名更に変更、９番のマイグレーション書き換え

--- conf/db/migration/V9__add_rate.sql
+++ conf/db/migration/V9__add_rate.sql
@@ -1,3 +1,3 @@
-alter table "review" add star double;
-alter table "review" add fun double;
-alter table "review" add hospitality double;
+alter table "review" add "star" double;
+alter table "review" add "fun" double;
+alter table "review" add "hospitality" double;
